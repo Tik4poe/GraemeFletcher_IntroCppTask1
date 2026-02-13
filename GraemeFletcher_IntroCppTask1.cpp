1df07b85c41cf5094852459efc65ba1fbc92d838
Te Poro <teporofletcher47@gmail.com>
2026-02-13 14:03:51 +1100

    I completed print array of task 1

--- GraemeFletcher_IntroCppTask1.cpp
+++ GraemeFletcher_IntroCppTask1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+//Global
+int userInt = 0;
+
 int main()
 {
-    
+	// 1) Print Array
+	int theArray[10] = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 };
+
+	int size = sizeof(theArray) / sizeof(theArray[0]);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << theArray[i] << ",";
+	}
+	cout << "\n\n==================================================" << endl;
+
+
 }
